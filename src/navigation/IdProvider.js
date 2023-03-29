d9df4e2c25dab428c09cb1f910df0dd205c6a2f1
0xpwnstar <chatna717@gmail.com>
2023-03-29 15:52:14 +0530

    added imageKey hook

---
+++ src/navigation/IdProvider.js
@@ -0,0 +1,15 @@
+import React, { createContext, useState} from 'react';
+
+export const IdContext = createContext()
+
+export const IdProvider = ({children}) => {
+    const [id, setId] = useState(null);
+
+    return (
+        <IdContext.IdProvider
+        value={{
+            id,
+            setId
+        }}
+    )
+}
